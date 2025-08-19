0066240666788d26e56176c850e46f223a1a75f6
Gutierrezmauro <gutierrezmaur8@gmail.com>
2025-08-19 20:47:38 -0300

    Primer comit del Amigo-Secreto

---
+++ challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
@@ -0,0 +1,12 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let ListaDeAmigoSecreto = [];
+
+function agregarAmigo() {
+    let valorAmigo = document.querySelector("#valorDelAmigo").value;
+    console.log(valorAmigo);
+}
+ 
+//function LimpiarCaja() {
+    //document.querySelector("#valorDelAmigo").value = "";
+//}
