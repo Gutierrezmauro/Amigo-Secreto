60204ee614683ee382e7a8ca1290c491a3caa4ba
Mauro Gutierrez <gutierrezmaur8@gmail.com>
2025-08-20 00:31:11 -0300

    Implementacion de una función para actualizar la lista de amigos

--- challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
@@ -2,6 +2,7 @@
 
 let ListaDeAmigoSecreto = [];
 let valorAmigo = "";
+let listaDeAmigo = "";
 
 
 function agregarAmigo() {
@@ -14,6 +15,26 @@ function agregarAmigo() {
 
    }console.log(ListaDeAmigoSecreto);
    valorAmigo = document.querySelector("#valorDelAmigo").value = "";
+   actualizarLista();
 }
+
+function actualizarLista() {
+ listaDeAmigo = document.getElementById("listaAmigos");
+ listaDeAmigo.innerHTML = "";
+
+        //ListaDeAmigoSecreto = ['Renault', 'Peugeot', 'Tesla'];
+    for (let i = 0; i < ListaDeAmigoSecreto.length; i++) {
+         let nuevoElemento = document.createElement("li");
+        nuevoElemento.textContent = ListaDeAmigoSecreto[i];
+        listaDeAmigo.appendChild(nuevoElemento);
+       //ListaDeAmigoSecreto[0] = "<li>" + ListaDeAmigoSecreto[i] + "</li>";
+        //listaDeAmigoSecreto[0] va a ser igual a renault
+        //console.log('Ma marque de voiture préférée est : ' + voiture[i]);
+}
+//listaDeAmigo.innerHTML = ListaDeAmigoSecreto
+} 
+//actualizarLista();
+
+
 
 
