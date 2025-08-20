a6388befeba0b64a81a61b8112778cef64695396
Mauro Gutierrez <gutierrezmaur8@gmail.com>
2025-08-19 21:56:36 -0300

    Implementacion de una función para agregar amigos

--- challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
@@ -1,12 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let ListaDeAmigoSecreto = [];
+let valorAmigo = "";
+
 
 function agregarAmigo() {
-    let valorAmigo = document.querySelector("#valorDelAmigo").value;
-    console.log(valorAmigo);
+      valorAmigo = document.querySelector("#valorDelAmigo").value;
+     console.log(valorAmigo);
+   if (valorAmigo == ""){
+    alert("Por favor, inserte un nombre"); 
+   } else{
+     ListaDeAmigoSecreto.push(valorAmigo);
+
+   }console.log(ListaDeAmigoSecreto);
+   valorAmigo = document.querySelector("#valorDelAmigo").value = "";
 }
- 
-//function LimpiarCaja() {
-    //document.querySelector("#valorDelAmigo").value = "";
-//}
+
+
